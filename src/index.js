e287025bd18990c4da4231858279557cc30074f6
Sascha Becker <s.becker@wertarbyte.com>
2016-11-05 11:19:40 +0100

    Add hapi redis scaffold.

---
+++ src/index.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const Hapi = require('hapi')
+const Inert = require('inert')
+const Vision = require('vision')
+
+const sessionRoutes = require('./routes/sessions')
+
+const server = new Hapi.Server()
+
+server.connection({
+  port: 3000
+})
+
+server.register([
+  Inert,
+  Vision,
+  {
+    'register': require('hapi-swagger'),
+    'options': {
+      info: {
+        'title': 'Test API Documentation'
+      }
+    }
+  },
+  {
+    'register': require('hapi-redis'),
+    'options': {
+      host: 'localhost',
+      opts: {
+        parser: 'javascript'
+      }
+    }
+  },
+  sessionRoutes
+], (err) => {
+  if (err) {
+    throw err
+  }
+  server.start((err) => {
+    if (err) {
+      throw err
+    }
+    console.log('Server running at:', server.info.uri)
+  })
+})
---
+++ src/routes/sessions.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Boom = require('boom')
+const uuid = require('node-uuid')
+const Joi = require('joi')
+const shortid = require('shortid')
+
+exports.register = (server, options, next) => {
+  const redisClient = server.plugins['hapi-redis'].client
+
+  server.route({
+    method: 'GET',
+    path: '/test',
+    handler: (request, reply) => {
+      reply("Hello world")
+    }
+  })
+
+
+  return next()
+}
+
+exports.register.attributes = {
+  name: 'routes-packages'
+}
