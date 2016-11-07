9c65cb271bad1c3cf5e1348156d0b9eef42c31ac
Sascha Becker <s.becker@wertarbyte.com>
2016-11-07 13:55:30 +0100

    Insert data into database.

--- src/routes/sessions.js
+++ src/routes/sessions.js
@@ -8,8 +8,20 @@ const shortid = require('shortid')
 const pcap = require("pcap")
 let pcap_session
 
+let counter = 0
+
 exports.register = (server, options, next) => {
   const redisClient = server.plugins['hapi-redis'].client
+
+  server.route({
+    method: 'GET',
+    path: '/data',
+    handler: (request, reply) => {
+      redisClient.hgetall("package:5", function (err, obj) {
+        reply(obj)
+      })
+    }
+  })
 
   server.route({
     method: 'GET',
@@ -35,8 +47,23 @@ exports.register = (server, options, next) => {
             length: tcpPackage.dataLength,
             data: tcpPackage.data
           }
+
+          redisClient.hmset(`package:${counter}`,
+            "saddr", result.saddr,
+            "daddr", result.daddr,
+            "sport", result.sport,
+            "dport", result.dport,
+            "timestamp", result.timestamp,
+            "data", result.data,
+            "datalength", result.length
+            , function (err, res) {})
+
+          counter++
+
+          console.log(result)
         }
       })
+
 
       reply(`Listening on ${pcap_session.device_name}`)
     }
